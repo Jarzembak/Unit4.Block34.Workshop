ddb6d8d998bc6cd6ca9a472cc5b957d19cd8771d
Jarzembak <lee.jarzembak@gmail.com>
2024-03-23 16:18:50 -0400

    finished db functions, seeded db

--- server/db.js
+++ server/db.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const pg = require("pg");
 const client = new pg.Client(process.env.DATABASE_URL);
 const uuid = require("uuid");
@@ -40,18 +41,24 @@ const createRestaurant = async (name) => {
     return (await client.query(SQL, [uuid.v4(), name])).rows[0];
 };
 
-const createReservation = async ({
+const createReservation = async (
     date,
     party_count,
     restaurant_id,
-    customer_id,
-}) => {
+    customer_id
+) => {
     const SQL = /*SQL*/ `
     INSERT INTO reservation(id, date, party_count, restaurant_id, customer_id)
-    VALUES ($1, $2, $3, $4) RETURNING *;
-    const response = await client.query(SQL, [uuid.v4(), date, party_count, restaurant_id, customer_id])
-    return respose.rows(0);
+    VALUES ($1, $2, $3, $4, $5) RETURNING *;
     `;
+    const response = await client.query(SQL, [
+        uuid.v4(),
+        date,
+        party_count,
+        restaurant_id,
+        customer_id,
+    ]);
+    return response.rows[0];
 };
 
 const destroyReservation = async ({ customer_id, id }) => {
@@ -69,4 +76,71 @@ const fetchRestaurants = async () => {
     const SQL = /*SQL*/ `SELECT * FROM restaurant;`;
     const response = await client.query(SQL);
     return response.rows;
+};
+
+const seed = async () => {
+    await Promise.all([
+        createCustomer("Lee"),
+        createCustomer("Brett"),
+        createCustomer("Matt"),
+        createCustomer("Laura"),
+        createCustomer("David"),
+        createRestaurant("Terry's Tavern"),
+        createRestaurant("Bob's Bistro"),
+        createRestaurant("Gill's Grill"),
+        createRestaurant("Dee's Diner"),
+        createRestaurant("Cora's Cafe"),
+    ]);
+
+    const customers = await fetchCustomers();
+    console.log("customers seeded", customers);
+    const restaurants = await fetchRestaurants();
+    console.log("restaurants seeded", restaurants);
+
+//     const makeReservations = [
+//             {
+//                 date: "2024-03-01",
+//                 party_count: 2,
+//                 restaurant_id: restaurants[0].id,
+//                 customer_id: customers[0].id,
+//             },
+//             {
+//                 date: "2024-03-02",
+//                 party_count: 4,
+//                 restaurant_id: restaurants[1].id,
+//                 customer_id: customers[1].id,
+//             },
+//             {
+//                 date: "2024-03-03",
+//                 party_count: 6,
+//                 restaurant_id: restaurants[2].id,
+//                 customer_id: customers[2].id,
+//             },
+//             {
+//                 date: "2024-03-04",
+//                 party_count: 8,
+//                 restaurant_id: restaurants[3].id,
+//                 customer_id: customers[3].id,
+//             },
+//             {
+//                 date: "2024-03-05",
+//                 party_count: 10,
+//                 restaurant_id: restaurants[4].id,
+//                 customer_id: customers[4].id,
+//             }
+//         ];
+//     makeReservations.map((reservation) =>
+//                 createReservation(
+//                     reservation.date,
+//                     reservation.party_count,
+//                     reservation.restaurant_id,
+//                     reservation.customer_id
+//                 )
+//             )
+};
+
+module.exports = {
+    client,
+    createTables,
+    seed
 };
